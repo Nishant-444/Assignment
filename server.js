41dae0f659d674689195987921b460f7b335588e
Nishant Sharma <197467920+Nishant-444@users.noreply.github.com>
2025-08-11 09:16:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
+	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 });
 
